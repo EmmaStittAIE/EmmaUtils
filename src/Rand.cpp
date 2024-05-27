c7a462cfbe6c1dedd8074039f52a9d3e7e020e52
Emma Stitt <emmcooke@optusnet.com.au>
2024-05-27 15:00:10 +1000

    Fixed segmentation fault bug

--- src/Rand.cpp
+++ src/Rand.cpp
@@ -7,7 +7,7 @@ const std::string Rand::GetRandomStringFromList(std::vector<std::string> listOfS
 	// Some <random> magic. Essentially, setup a seeded random generator, and give it minimum (inclusive) and maximum (exclusive) values
 	std::random_device rd;
 	std::mt19937 mt(rd());
-	std::uniform_int_distribution<std::mt19937::result_type> dist(0, listOfStrings.size());
+	std::uniform_int_distribution<std::mt19937::result_type> dist(0, listOfStrings.size()-1);
 
 	return listOfStrings[dist(mt)];
 }
